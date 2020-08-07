10cfa8ef26304e9ae2fa6ede27ee80bb3d369ade
Jackson Beytebiere <jacksongrowson@gmail.com>
2020-08-07 00:13:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(string)
   string_array = string.split
   reverse_array = string_array.collect {|word| word.reverse}
-end
   strreverse = reverse_array.join(' ')
   strreverse
 end
